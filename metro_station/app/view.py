9a305c8c4ed00bb65f8ebf5c6cde619e0f5afac8
Daniil Razumov <razumov.daniil@mail.ru>
2020-03-26 01:59:06 +0300

    add try-except

--- metro_station/app/view.py
+++ metro_station/app/view.py
@@ -5,24 +5,31 @@ import json
 
 @app.route('/api/v1/metro/verificate/', methods=["POST"])
 def sync_stations():
-    data = set(request.get_json())
-    full_json = requests.get('https://api.hh.ru/metro/1').json()
-    base_stations = set()
-    for lines in full_json['lines']:
-        for station in lines['stations']:
-            base_stations.add(station['name'])
+    try:
+        data = set(request.get_json())
+        full_json = requests.get('https://api.hh.ru/metro/1').json()
+        base_stations = set()
+        for lines in full_json['lines']:
+            for station in lines['stations']:
+                base_stations.add(station['name'])
 
-    unchanged = data & base_stations
-    updated = data - base_stations
-    deleted = base_stations - data
+        unchanged = data & base_stations
+        updated = data - base_stations
+        deleted = base_stations - data
 
-    final_json = {"unchanged": list(unchanged),
-                  "updated": list(updated),
-                  "deleted": list(deleted)}
-    response = app.response_class(
-        response=json.dumps(final_json),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
+        final_json = {"unchanged": list(unchanged),
+                    "updated": list(updated),
+                    "deleted": list(deleted)}
+        response = app.response_class(
+            response=json.dumps(final_json),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+    except Exception as ex:
+        return app.response_class(
+            response=str(ex),
+            status=400,
+        )
+
 
