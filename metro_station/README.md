8ba8bec1284700a29214f4ffe2f328645e6798b3
Daniil Razumov <razumov.daniil@mail.ru>
2020-03-26 01:46:01 +0300

    create metro stations sync

---
+++ metro_station/README.md
@@ -0,0 +1,3 @@
+Необходимо запустить flask приложение на localhost \
+Дернуть url http://127.0.0.1:5000/api/v1/metro/verificate/ по POST запросу, необоходимо указать Header: Content-Type:application/json \
+В тело запроса передать массив из необходимых станций метро
---
+++ metro_station/app/view.py
@@ -0,0 +1,28 @@
+from app import app
+from flask import request
+import requests
+import json
+
+@app.route('/api/v1/metro/verificate/', methods=["POST"])
+def sync_stations():
+    data = set(request.get_json())
+    full_json = requests.get('https://api.hh.ru/metro/1').json()
+    base_stations = set()
+    for lines in full_json['lines']:
+        for station in lines['stations']:
+            base_stations.add(station['name'])
+
+    unchanged = data & base_stations
+    updated = data - base_stations
+    deleted = base_stations - data
+
+    final_json = {"unchanged": list(unchanged),
+                  "updated": list(updated),
+                  "deleted": list(deleted)}
+    response = app.response_class(
+        response=json.dumps(final_json),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
